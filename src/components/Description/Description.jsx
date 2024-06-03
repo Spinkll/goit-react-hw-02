f20a6fe0ddd5583e318b9f9cf1fba9ff072dc9a0
Spinkll <roma.skorohod.2005@gmail.com>
2024-06-03 15:23:31 +0300

    task completed

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,13 @@
+import css from "./Description.module.css";
+
+export const Description = () => {
+  return (
+    <div>
+      <h1 className={css.main_text}>Sip Happens Café</h1>
+      <h2>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </h2>
+    </div>
+  );
+};
